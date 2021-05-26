2bd3d443dc760944e7d523b1ed8f143149a4aec4
Matheus094-tech <80859363+Matheus094-tech@users.noreply.github.com>
2021-05-26 16:09:24 -0300

    Blog Pessoal

---
+++ src/main/java/com/generation/blogPessoalMatheus/BlogPessoalMatheusApplication.java
@@ -0,0 +1,13 @@
+package com.generation.blogPessoalMatheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPessoalMatheusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogPessoalMatheusApplication.class, args);
+	}
+
+}
